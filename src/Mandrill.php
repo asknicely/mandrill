e57a5b39ec0b487333bd6bc1da39e87107dc5716
Filmon Asgodom <59848306+askfilmon@users.noreply.github.com>
2024-08-21 15:43:10 +1200

    Revert "WEB-14680 cURL set HTTP version to 1.1"

--- src/Mandrill.php
+++ src/Mandrill.php
@@ -86,7 +86,6 @@ class Mandrill {
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, 30);
         curl_setopt($this->ch, CURLOPT_TIMEOUT, 600);
-        curl_setopt($this->ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 
         $this->root = rtrim($this->root, '/') . '/';
 
